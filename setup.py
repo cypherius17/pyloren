dea3ec43ccc501530fe019e2c6445f754477edc5
cypherius <29733949+cypherius17@users.noreply.github.com>
2019-09-03 10:08:49 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyloren",
-    version="0.0.1",
+    version="1.0.0",
     author="cypherius",
     description="Generate Python package layout in one command",
     license="MIT",
